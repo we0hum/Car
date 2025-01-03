d0b40e0f6c29328ff86a116f40ab79ae33ab36b8
we0hum <emil.murzaliev.00@mail.ru>
2025-01-03 18:10:05 +0400

    запись и чтение из файла

---
+++ src/test_rectangle/WriteToFile.java
@@ -0,0 +1,47 @@
+package test_rectangle;
+
+import java.util.Random;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WriteToFile {
+    public static void main(String[] args) {
+        BufferedReader br = null;
+        try {
+            File file = new File("TestWriteToFile");
+            PrintWriter pw = new PrintWriter(file);
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 1; j++) {
+                    pw.println(random.nextInt(100));
+                }
+            }
+            pw.close();
+
+            br = new BufferedReader(new FileReader("TestWriteToFile"));
+            String line;
+            int a = 1;
+            if (a != 0) {
+                while ((line = br.readLine()) != null) {
+                    System.out.print("Значение " + a + " равно: ");
+                    System.out.println(line);
+                    a++;
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Ошибка: " + e);
+        }
+        finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка: " + e);
+            }
+        }
+    }
+}
